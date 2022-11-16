bfa20cb43ca2d0626c26a7ffd7c9239b210557ea
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 15:18:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import streamlit as st
 url = 'https://www.indeed.com/jobs?q=web+developer&l=New+York'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-#results = soup.find(id='resultsCol')
+results = soup.find(p)
 print(soup.prettify())
 st.write(soup.prettify())
 
