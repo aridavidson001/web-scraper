5b9cbd0faba9c9adc5d0cd49814ec38f2dd280d7
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 18:05:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ url = 'http://api.scraperapi.com?api_key=f894cc3b0ca8e6b70619aa940b31139a&url=ht
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 print (soup.find(id='resultsCol'))
-results = soup.find(id='resultsCol')
+results = (soup.find(id='resultsCol')
 indeed_jobs = results.select('div.jobsearch-SerpJobCard.unifiedRow.row.result')
 file = open('indeed-jobs.csv', 'w')
 writer = csv.writer(file)
