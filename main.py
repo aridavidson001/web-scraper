1656a9c6fac5d6edf09e324de905e5bf41c2dfd8
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 08:59:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import streamlit as st
+from bs4 import BeautifulSoup
+
 url = 'https://www.indeed.com/jobs?'
 page = requests.get(url)
-st.write(page.content)
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soupfind(id='resultsCol')
+st.write(results.prettify())
