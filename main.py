dadc013ddd93d0bf0b195a4276ba0bb9a95f3f91
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 15:53:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,8 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
-url = 'https://www.indeed.com/jobs?q=web+developer&l=New+York'
-page = requests.get(url)
-print 'hello'
-soup = BeautifulSoup(page.content, 'html.parser')
-print 'hello'
-print(soup.findAll('link', limit=10))
-print 'hello'
-#results = soup.find('link')
-#print(results)
-#st.write(soup)
-#print BeautifulSoup(soup).find('p').text
 
-'''
-url = 'https://www.indeed.com/jobs?q=web+developer&l=New+York'
-page = requests.get(url)
-st.write(page.content)
-st.write(page.status_code)
-'''
-
-
-
-'''url = 'http://api.scraperapi.com?api_key=f894cc3b0ca8e6b70619aa940b31139a&url=https://www.indeed.com/jobs?q=web+developer&l=New+York'
+url = 'http://api.scraperapi.com?api_key=f894cc3b0ca8e6b70619aa940b31139a&url=https://www.indeed.com/jobs?q=web+developer&l=New+York'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='resultsCol')
@@ -39,4 +19,4 @@ for indeed_job in indeed_jobs:
    job_url = indeed_job.find('a')['href']
    writer.writerow([job_title.encode('utf-8'), job_company.encode('utf-8'), job_location.encode('utf-8'), job_url.encode('utf-8')])
 file.close()
-'''
+
