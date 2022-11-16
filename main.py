eebec41c9dfc4c5a3dd8f874aed1343cb380366f
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 14:04:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-import requests
+import 
 from bs4 import BeautifulSoup
 import streamlit as st
 
@@ -7,7 +7,7 @@ url = 'http://api.scraperapi.com?api_key=f894cc3b0ca8e6b70619aa940b31139a&url=ht
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='resultsCol')
-indeed_jobs = results.select('div.jobsearch-SerpJobCard.unifiedRow.row.result')
+indeed_jobs = requests.select('div.jobsearch-SerpJobCard.unifiedRow.row.result')
 file = open('indeed-jobs.csv', 'w')
 writer = csv.writer(file)
 # write header rows
