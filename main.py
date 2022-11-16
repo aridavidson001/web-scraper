e369212fd77100c7c59cad5d5591f472895c9960
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 18:09:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 print (soup.find(id='resultsCol'))
 results = (soup.find(id='resultsCol')
-indeedjobs = results.select('div.jobsearch-SerpJobCard.unifiedRow.row.result')
+indeed_jobs = (results.select('div.jobsearch-SerpJobCard.unifiedRow.row.result')
 file = open('indeed-jobs.csv', 'w')
 writer = csv.writer(file)
 # write header rows
 writer.writerow(['Title', 'Company', 'Location', 'Apply'])
-for indeedjob in indeed_obs:
-   job_title = indeedjob.find('h2', class_='title').text.strip()
-   job_company = indeedjob.find('span', class_='company').text.strip()
-   job_location = indeedjob.find('span', class_='location accessible-contrast-color-location').text.strip()
-   job_url = indeedjob.find('a')['href']
+for indeed_job in indeed_jobs:
+   job_title = indeed_job.find('h2', class_='title').text.strip()
+   job_company = indeed_job.find('span', class_='company').text.strip()
+   job_location = indeed_job.find('span', class_='location accessible-contrast-color-location').text.strip()
+   job_url = indeed_job.find('a')['href']
    writer.writerow([job_title.encode('utf-8'), job_company.encode('utf-8'), job_location.encode('utf-8'), job_url.encode('utf-8')])
 file.close()
 
