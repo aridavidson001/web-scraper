d946ba288a8f77fd0cfec52fe7b9a12a705bc491
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 15:47:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import streamlit as st
 url = 'https://www.indeed.com/jobs?q=web+developer&l=New+York'
 page = requests.get(url)
+print('hello')
 soup = BeautifulSoup(page.content, 'html.parser')
 print('hello')
 print(soup.findAll('link', limit=10))
