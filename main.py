e3beeb4b790d2d16a323f81970ebdba2d067fa10
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 18:03:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import streamlit as st
 url = 'http://api.scraperapi.com?api_key=f894cc3b0ca8e6b70619aa940b31139a&url=https://www.indeed.com/jobs?q=web+developer&l=New+York'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-print (soup.find_all('id='resultsCol'))
-results = soup.find_all('id='resultsCol')
+print (soup.find_all(id='resultsCol'))
+results = soup.find_all(id='resultsCol')
 indeed_jobs = results.select('div.jobsearch-SerpJobCard.unifiedRow.row.result')
 file = open('indeed-jobs.csv', 'w')
 writer = csv.writer(file)
