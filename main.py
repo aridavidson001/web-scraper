0a4fbaa1f20ad7fbe772b0dd972686265de9fa20
Inigo-montoya01 <66476782+aridavidson001@users.noreply.github.com>
2022-11-16 09:02:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import requests
 import streamlit as st
 from bs4 import BeautifulSoup
 
-url = 'https://www.indeed.com/jobs?'
+
+url = 'https://www.indeed.com/jobs?q=web+developer&l=New+York'
+
 page = requests.get(url)
+
 soup = BeautifulSoup(page.content, 'html.parser')
+
 results = soup.find(id='resultsCol')
+
 print(results.prettify())
 st.write(results.prettify())
